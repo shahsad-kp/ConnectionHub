afd9a5904b37e42fd14ec643d7daaaa02cfa9d09
Shahsad <shahsadkpklr@gmail.com>
2023-03-05 13:14:24 +0530

    Admin Help messages

--- AdminMessages/models.py
+++ AdminMessages/models.py
@@ -7,5 +7,14 @@ class HelpMessage(models.Model):
     message = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
 
+    def get_context(self):
+        return {
+            'id': self.id,
+            'user': self.user.get_context(),
+            'subject': self.subject,
+            'message': self.message,
+            'date_created': self.date_created.strftime('%Y-%m-%d %H:%M'),
+        }
+
     def __str__(self):
         return f'{self.user.username} - {self.subject}'
--- AdminMessages/views.py
+++ AdminMessages/views.py
@@ -11,22 +11,7 @@ def admin_messages_page(request: HttpRequest):
     messages = HelpMessage.objects.all()
     data = {
         'messages': [
-            {
-                'id': message.id,
-                'user': {
-                    'username': message.user.username,
-                    'profile_picture': message.user.profile_picture.url,
-                    'url': reverse(
-                        viewname='admin-profile-pages',
-                        args=[
-                            message.user.username
-                        ]
-                    )
-                },
-                'subject': message.subject,
-                'message': message.message,
-                'date_created': message.date_created,
-            }
+            message.get_context()
             for message in messages
         ]
     }
