0db6a0f511cfc388f37aeda688bf89eda2d2b4e1
Shahsad KP <shahsadkpklr@gmail.com>
2023-03-23 13:21:38 +0530

    Delete admin directory

--- admin/AdminHelp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdminhelpConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Admin.AdminHelp'
--- admin/AdminHome/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import admin_home, admin_login, admin_logout
-
-urlpatterns = [
-    path('', admin_home, name='admin-home'),
-    path('login/', admin_login, name='admin-login'),
-    path('logout/', admin_logout, name='admin-logout'),
-]
