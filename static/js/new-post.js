05b1cf9b5058276e3be88169b8b5dfbf15751d5f
Shahsad <shahsadkpklr@gmail.com>
2023-03-26 21:44:00 +0530

    Added tags adding options and showing in posts

--- static/js/new-post.js
+++ static/js/new-post.js
@@ -1,5 +1,5 @@
 $(document).ready(
-    function(){
+    function () {
         const fileInput = $('#file-input');
         const imagePreview = $('#image-preview');
         const submitButton = $('#submit-button');
@@ -14,23 +14,27 @@ $(document).ready(
                 alert('Please select an image');
                 return;
             }
+            if (!validateTags()) {
+                return alert('Please correct the hashtags');
+            }
             const form = new FormData();
             form.append('image', fileInput[0].files[0]);
             form.append('caption', caption.val());
             form.append('tags', tags.val());
             form.append('location', location.val());
             form.append('csrfmiddlewaretoken', csrfTokken);
-
+            submitButton.text('Uploading...').prop('disabled', true);
             $.ajax({
                 url: newPostUrl,
                 type: 'POST',
                 data: form,
                 processData: false,
                 contentType: false,
-                success: function(response) {
+                success: function (response) {
                     window.location.href = response.redirect;
                 },
-                error: function(response) {
+                error: function (response) {
+                    submitButton.text('Submit').prop('disabled', false);
                     console.log(response);
                 }
             });
@@ -40,7 +44,7 @@ $(document).ready(
             if (file.type.match('image.*')) {
                 const reader = new FileReader();
 
-                reader.onload = function(event) {
+                reader.onload = function (event) {
                     imagePreview.attr('src', event.target.result);
                 };
 
@@ -48,19 +52,40 @@ $(document).ready(
             }
         }
 
-        fileInput.change(function() {
+        function validateTags() {
+            if (tags.val().length > 0) {
+                let pattern = /^#[a-zA-Z0-9]+(\s+#[a-zA-Z0-9]+)*$/;
+                if (tags.val().replace(/^\s+|\s+$/g, '').match(pattern)) {
+                    $('#error-message').hide()
+                    return true;
+                } else {
+                    $('#error-message').text('Hashtags must be separated by spaces, start with #, and contain only letters and numbers.').css('color', 'red').show();
+                    return false;
+                }
+            }
+            else{
+                $('#error-message').hide()
+                return true;
+            }
+        }
+
+        fileInput.change(function () {
             displayImage(this.files[0]);
         });
 
-        imagePreview.on('dragover', function(event) {
+        imagePreview.on('dragover', function (event) {
             event.preventDefault();
         });
 
-        imagePreview.on('drop', function(event) {
+        imagePreview.on('drop', function (event) {
             event.preventDefault();
             displayImage(event.originalEvent.dataTransfer.files[0]);
         });
 
         submitButton.click(submitPost);
+        tags.on(
+            'input',
+            validateTags
+        )
     }
 )
