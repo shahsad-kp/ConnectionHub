8da810d53f448a855911f7896fc025e0989706d1
Shahsad KP <shahsadkpklr@gmail.com>
2023-03-28 09:37:31 +0000

    Debug turned to off

--- ConnectionHub/settings.py
+++ ConnectionHub/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['connectionhub.live', 'www.connectionhub.live', 'connectionhub.social', 'www.connectionhub.social']
 
