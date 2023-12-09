4627a4aa15f2593c7967c40e9eb3e77087a66c04
Shahsad KP <shahsadkpklr@gmail.com>
2023-12-09 23:51:43 +0530

    feature: Resend email verification api created
    - Modified Verify user email api with Token class

--- backend/Account/urls.py
+++ backend/Account/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register_view'),
     path('me/', CurrentUserView.as_view(), name='get_current_user'),
     path('update/', UpdateUserView.as_view(), name='update_user_details'),
-    path('verify/<uuid:user_id>/<str:token>/', VerifyUserEmail.as_view(), name='verify_user_email')
+    path('verify/<str:token>/', VerifyUserEmail.as_view(), name='verify_user_email')
 ]
