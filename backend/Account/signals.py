46f0e77389f4b0b20c6473cf51f3101cdbeca423
Shahsad KP <shahsadkpklr@gmail.com>
2023-12-09 23:54:01 +0530

    fix: Signals for sending verification email
    - Added Profile creation for each user
    - Added signal to send verification email whenever user changes email

--- backend/Account/signals.py
+++ backend/Account/signals.py
@@ -1,24 +1,25 @@
-from django.conf import settings
-from django.contrib.auth.tokens import default_token_generator
-from django.core.mail import send_mail
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 
 from Account.models import User
+from Profile.models import Profile
 
 
 @receiver(post_save, sender=User)
-def send_email_verification_link(_: type[User], instance: User, created: bool, **__):
+def user_post_signal(sender: type[User], instance: User, created: bool, **kwargs):
     if created:
-        token = default_token_generator.make_token(instance)
-        subject = 'Welcome to ConnectionHub world'
-        message = f'Hi {instance.username}, thank you for registering in ConnectionHub. Your Token is: {token}.'
-        email_from = settings.EMAIL_HOST_USER
-        recipient_list = [instance.email]
-        send_mail(
-            subject=subject,
-            message=message,
-            from_email=email_from,
-            recipient_list=recipient_list,
-            fail_silently=False
+        instance.send_verification_email()
+        Profile.objects.create(
+            user=instance
         )
+
+
+@receiver(pre_save, sender=User)
+def user_pre_signal(sender: type[User], instance: User, **kwargs):
+    try:
+        original_user = User.objects.get(pk=instance.pk)
+        if instance.email != original_user.email:
+            instance.is_verified = False
+            instance.send_verification_email()
+    except User.DoesNotExist:
+        pass
