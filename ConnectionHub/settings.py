87c255ff7f8203a868b4b1841e89d976dfff895a
Shahsad KP <shahsadkpklr@gmail.com>
2024-12-01 12:55:15 +0530

    Added static files root

--- ConnectionHub/settings.py
+++ ConnectionHub/settings.py
@@ -161,6 +161,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files
 MEDIA_URL = 'media/'
